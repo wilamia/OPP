b7d3b5903ceb5ab35337bce50f806a0642042603
Kseniya <eseniya.wi@gmail.com>
2025-01-17 18:29:54 +0300

    did lab13

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
+    interface MathOperation {
+        int sumOfNumbers(int[] numbers);
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        MathOperation op = (int[] numbers) -> {
+            int sum = 0;
+            for (int i=0; i<numbers.length; i++){
+                numbers[i] = scan.nextInt();
+                if(numbers[i]%4==0 && numbers[i]%10==6) {
+                    sum+=numbers[i];
+                }
+            }
+            return sum;
+        };
+
+        System.out.println("Enter numbers: ");
+
+        int[] numbers = new int[5];
+        int result = op.sumOfNumbers(numbers);
+
+        System.out.println("Result: " + result);
+    }
 }
