8d9250ba77609cf91e4ea4cc1327c8fa76b2281c
Kseniya <eseniya.wi@gmail.com>
2025-02-28 14:00:37 +0300

    lab17

--- src/Main.java
+++ src/Main.java
@@ -8,63 +8,74 @@ public class Main {
             this.message = message;
         }
 
-       void decryption(String message, int shiftNumber) {
-            StringBuilder new_str = new StringBuilder();
-            int c;
-            int y;
+        void decryption(String message, int shiftNumber) {
+            synchronized (this) {
+                System.out.println(Thread.currentThread().getName() + " decrypting: " + message);
+                try {
+                    StringBuilder new_str = new StringBuilder();
+                    int c;
+                    int y;
 
-            for (int i = 0; i < message.length(); i++) {
-                c = (message.charAt(i) - shiftNumber);
-                y = (c - (int) message.charAt(i)) % 32 + (int) message.charAt(i);
-                new_str.append((char) y);
+                    for (int i = 0; i < message.length(); i++) {
+                        c = (message.charAt(i) - shiftNumber);
+                        y = (c - (int) message.charAt(i)) % 32 + (int) message.charAt(i);
+                        new_str.append((char) y);
+                    }
+
+                    System.out.println("Crypted: " + message + " into " + new_str.toString());
+                    this.message = new_str.toString();
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-
-            System.out.println("Crypted: " + message + " into "+new_str.toString());
-           this.message = new_str.toString();
         }
 
-         void encryption(String message, int shiftNumber) {
-            StringBuilder new_str = new StringBuilder();
-            int c;
-            int y;
-            for (int i = 0; i < message.length(); i++) {
-                c = (message.charAt(i) + shiftNumber);
-                y = (c - (int) message.charAt(i)) % 32 + (int) message.charAt(i);
-                new_str.append((char) y);
+        void encryption(String message, int shiftNumber) {
+            synchronized (this) {
+                System.out.println(Thread.currentThread().getName() + " encrypting: " + message);
+                try {
+                    StringBuilder new_str = new StringBuilder();
+                    int c;
+                    int y;
+                    for (int i = 0; i < message.length(); i++) {
+                        c = (message.charAt(i) + shiftNumber);
+                        y = (c - (int) message.charAt(i)) % 32 + (int) message.charAt(i);
+                        new_str.append((char) y);
+                    }
+                    System.out.println("Encrypted: " + message + " into " + new_str.toString());
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-            System.out.println("Encrypted: " + message + " into " + new_str.toString());
         }
     }
 
     public static void main(String[] args) {
+        CaesarCipher cipher1 = new CaesarCipher(3, "House");
+        CaesarCipher cipher2 = new CaesarCipher(12, "Walk");
+
         Thread thread1 = new Thread(() -> {
-            CaesarCipher cipher1 = new CaesarCipher(3, "House");
-            cipher1.decryption(cipher1.message, cipher1.shift);
-            cipher1.encryption(cipher1.message, cipher1.shift);
-        });
+            synchronized (cipher1) {
+                cipher1.decryption(cipher1.message, cipher1.shift);
+                try { Thread.sleep(50); } catch (InterruptedException e) { e.printStackTrace(); }
+                synchronized (cipher2) {
+                    cipher2.encryption(cipher2.message, cipher2.shift);
+                }
+            }
+        }, "Thread-1");
+
         Thread thread2 = new Thread(() -> {
-            CaesarCipher cipher2 = new CaesarCipher(12, "Walk");
-            cipher2.decryption(cipher2.message, cipher2.shift);
-            cipher2.encryption(cipher2.message, cipher2.shift);
-        });
-        Thread thread3 = new Thread(() -> {
-            CaesarCipher cipher3 = new CaesarCipher(16, "Summer");
-            cipher3.decryption(cipher3.message, cipher3.shift);
-            cipher3.encryption(cipher3.message, cipher3.shift);
-        });
-        Thread thread4 = new Thread(() -> {
-            CaesarCipher cipher4 = new CaesarCipher(27, "Autumn");
-            cipher4.decryption(cipher4.message, cipher4.shift);
-            cipher4.encryption(cipher4.message, cipher4.shift);
-        });
+            synchronized (cipher2) {
+                cipher2.decryption(cipher2.message, cipher2.shift);
+                try { Thread.sleep(50); } catch (InterruptedException e) { e.printStackTrace(); }
+                synchronized (cipher1) {
+                    cipher1.encryption(cipher1.message, cipher1.shift);
+                }
+            }
+        }, "Thread-2");
         thread1.start();
         thread2.start();
-        thread3.start();
-        thread4.start();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 }
